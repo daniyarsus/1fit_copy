43598816b6e17a2ed043f491cb0dd4cd7e120a5f
daniyarsus <daniyar.kanu@gmail.com>
2024-03-13 19:19:48 +0500

    add arch

---
+++ src/api/db/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from src.api.settings.config import settings
+
+async_engine = create_async_engine(
+    url=settings.pg_database.POSTGRES_DATABASE_URL,
+    echo=False
+)
+
+async_session = async_sessionmaker(
+    async_engine,
+    expire_on_commit=False,
+    autocommit=False,
+)
+
+
+class Base(DeclarativeBase):
+    @property
+    def pk(self):
+        """Return the primary key value of the object"""
+        return getattr(self, self.__mapper__.primary_key[0].name)
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__}(id={self.pk})>'
