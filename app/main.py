49ce750f5821ce9b8448557c5e6d3a29af6e51e8
daniyarsus <daniyar.kanu@gmail.com>
2024-03-22 15:39:19 +0500

    edit main

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -6,7 +10,25 @@ from app.settings.db.connection import async_engine, Base
 from app.api.routers import all_routers
 
 
-app = FastAPI()
+async def create_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def delete_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables()
+    print("База готова")
+    yield
+    print("База очищена")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 app.add_middleware(
@@ -18,15 +40,6 @@ app.add_middleware(
 )
 
 
-@app.on_event("startup")
-async def startup() -> None:
-    try:
-        async with async_engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
-    except asyncio.exceptions.CancelledError:
-        print("Сервер остановлен.")
-
-
 @app.get("/")
 async def index():
     return {"message": "Хелоу :3"}
@@ -34,3 +47,6 @@ async def index():
 
 for router in all_routers:
     app.include_router(router, prefix='/api/v1', tags=['Auth endpoints - API'])
+
+if __name__ == "__main__":
+    uvicorn.run(app)
