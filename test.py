2d41f4ffd2617c7cf2163e65fa2410788a453fba
daniyarsus <daniyar.kanu@gmail.com>
2024-03-22 15:26:20 +0500

    edit versions in req.txt

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJzdHJpbmciLCJlbWFpbCI6InVzZXJAZXhhbXBsZS5jb20iLCJwaG9uZSI6ODc3NTcxMTE4NDUsImV4cCI6MzkwMzU4NDY0Mjl9.R-wXsG08bq8WU-9tA-zYozLh-r-r8M179uKYKf5xEFE"
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJzdHJpbmciLCJlbWFpbCI6InVzZXJAZXhhbXBsZS5jb20iLCJwaG9uZSI6ODc3NTcxMTE4NDUsImV4cCI6MzkwMzU4OTU2ODl9.vdxlXhrkl7s-AJvuO7FPMg9Hi-55QzrFYNzaYI5K3lE"
 
 # Заголовок запроса с токеном JWT
 headers = {
@@ -9,14 +9,9 @@ headers = {
 
 
 # URL защищенного эндпоинта
-url = "http://127.0.0.1:8000/api/v1/test/protected"
+url = "http://127.0.0.1:8000/api/v1/protected"
 
 # Отправка запроса POST к защищенному эндпоинту с использованием токена JWT и отправкой JSON данных
 response = requests.get(url, headers=headers)
 
-# Обработка ответа
-if response.status_code == 200:
-    print("Запрос успешно выполнен!")
-    print("Ответ сервера:", response.json())
-else:
-    print("Ошибка при выполнении запроса:", response.status_code)
+print(response.json(), response.status_code)
