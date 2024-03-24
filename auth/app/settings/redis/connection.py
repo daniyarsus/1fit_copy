9426e4b0028102129f9125fd7840ca3445e2d7ca
daniyarsus <daniyar.kanu@gmail.com>
2024-03-25 01:03:19 +0500

    add directory auth

--- auth/app/settings/redis/connection.py
+++ auth/app/settings/redis/connection.py
@@ -2,4 +2,4 @@ from redis import asyncio as redis
 
 from app.settings.config import settings
 
-redis_client_auth = redis.from_url(settings.regis_config.REDIS_URL_AUTH)
+redis_client_auth = redis.from_url(settings.regis_config.REDIS_URL)
--- auth/tests/auth.py
+++ auth/tests/auth.py
@@ -1,6 +1,5 @@
-import pytest
 from fastapi.testclient import TestClient
-from app.main import app
+from app import app
 
 
 client = TestClient(app)
